919eca8c9352741f374bc0a357c46cb471282ba7
eggplants <w10776e8w@yahoo.co.jp>
2020-06-24 14:08:25 +0900

    goods controller updated

--- app/controllers/api/v1/goods_controller.rb
+++ app/controllers/api/v1/goods_controller.rb
@@ -6,7 +6,27 @@ module Api
         goods = Good.order('created_at DESC');
         render json: {status: 'SUCCESS', message: 'Loaded Goods', data: goods}, status: :ok
       end
+      def show
+        good =  Good.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded Good: ' + params[:id], data: good}, status: :ok
+      end
+      def create
+        good = Good.new(good_params)
+        if good.save
+          render json: {status: 'SUCCESS', message: 'Saveed Good', data: good}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Failed to save Good', data: good.errors}, status: :unprocessable_entity
+        end
+      end
+      private
+
+      def article_params
+        params.permit(
+            :name, :shopid, :description, :data,
+            :price, :category, :link, :thumbnail, :date
+        )
+      end
+
     end
   end
 end
-
