8533b1c06319fd99d98031551ba54be87681a36d
eggplants <w10776e8w@yahoo.co.jp>
2020-06-26 16:02:35 +0900

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
-5.times do
+100.times do
   Good.create({
-  name: Faker::Book.title, shopid: rand(5),
-  description: "aaa", data: "hello.com/1",
-  price: [*10..1000].sample(1)[0], category: Faker::Cosmere.shard,
-  link:'aaa.com', thumbnail: 'bbb.com'
+  name: Faker::Book.title,
+  shopid: rand(5),
+  description: Faker::Lorem.sentences,
+  data: "hello.com/1",
+  price: [*10..1000].sample(1)[0],
+  category: Faker::Cosmere.shard,
+  link: Faker::Internet.domain_name,
+  thumbnail: Faker::Omniauth.google[:info][:image]
   })
 end
