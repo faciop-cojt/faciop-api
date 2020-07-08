8fde4b946cbcd5e7be06ed41a76f3e14cd516664
eggplants <w10776e8w@yahoo.co.jp>
2020-07-08 15:03:35 +0900

    update

--- app/controllers/api/v1/model_controller.rb
+++ app/controllers/api/v1/model_controller.rb
@@ -2,12 +2,14 @@ module Api
   module V1
     class ModelController < ApplicationController
       protect_from_forgery :only => ["create"]
+
       def create
         @model = Model.new(model_params)
         if @model.save
           render json: {
             status: 'SUCCESS', message: 'Saved Good',
             data: {
+              id: @model.id,
               goodid: @model.goodid,
               data:  @model.data.url,
             }
@@ -15,7 +17,7 @@ module Api
           status: :ok
         else
           render json: {
-            status: 'SUCCESS', message: 'Saved Good',
+            status: 'FAILED', message: 'Saved Poor',
           },
           status: :unprocessable_entity
         end
@@ -23,11 +25,22 @@ module Api
 
       def show
         @model = Model.where(goodid: params[:id])
-        render json: {
-          status: 'SUCCESS', message: 'Loaded Good',
-          data: @model.to_a.map(&:class).to_s
-        },
-        status: :ok
+        if @model.size > 0 
+          render json: {
+            status: 'SUCCESS', message: 'Loaded Good',
+            data: {
+              id: @model[0].id,
+              goodid: @model[0].goodid,
+              data: @model[0].data.url
+            }
+          },
+          status: :ok
+        else
+          render json: {
+            status: 'FAILED', message: 'Loaded Poor',
+          },
+          status: 404
+        end
       end
 
       private
