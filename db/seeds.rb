ecf613da214a7e7514ed0d1ad87dfee8a6d605b3
eggplants <w10776e8w@yahoo.co.jp>
2020-07-22 11:47:13 +0900

    Update seedf

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,31 @@
-100.times do
-  Good.create({
-  name: Faker::Book.title,
-  shopid: rand(5),
-  description: Faker::Lorem.sentences,
-  data: ["hello.com/1", ""][rand(2)],
-  price: [*10..1000].sample(1)[0],
-  category: ["メガネ", "帽子", "マスク"].sample(1)[0],
-  link: Faker::Internet.domain_name,
-  thumbnail: Faker::Omniauth.google[:info][:image]
-  })
-end
+Good.create({
+  name: 'Funny glasses',
+  shopid: '1',
+  description: 'イかした眼鏡',
+  data: '',
+  price: '12000',
+  category: 'メガネ',
+  link: 'https://www.amazon.co.jp/',
+  thumbnail: 'https://3.bp.blogspot.com/-330gH9GDMVY/V5jKcQxFYTI/AAAAAAAA81M/z31aAMRMWP0PO1xtX72JHIF6G7ZoFm2CQCLcB/s800/megane_wellington_man.png'
+})
+Good.create({
+  name: 'Cool hat',
+  shopid: '2',
+  description: 'クールな帽子',
+  data: '',
+  price: '3200',
+  category: '帽子',
+  link: 'https://www.amazon.co.jp/',
+  thumbnail: 'https://1.bp.blogspot.com/-kpsPxxojlAo/UdEea15-maI/AAAAAAAAVvA/eg5vYG6Tvno/s601/fashion_mugiwaraboushi.png'
+})
+Good.create({
+  name: '不織布マスク',
+  shopid: '3',
+  description: '薄いマスク',
+  data: '',
+  price: '100',
+  category: 'マスク',
+  link: 'https://www.amazon.co.jp/',
+  thumbnail: 'https://1.bp.blogspot.com/-tEEP6DMBBm4/Xtt6eQsD1JI/AAAAAAABZQQ/-bIHUTVF7gU9g8zGtfZfIzWkPuh5kKc1ACNcBGAsYHQ/s1600/medical_mask_front_view.png'
+})
+
