4c98d9288244543377a068f3f460745c18c290b3
eggplants <w10776e8w@yahoo.co.jp>
2020-07-28 17:50:17 +0900

    some data added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
+i = 0
 Good.create({
   name: 'Funny glasses',
-  shopid: '1',
+  shopid: (i+=1).to_s,
   description: 'イかした眼鏡',
   data: '',
   price: '12000',
@@ -10,7 +11,7 @@ Good.create({
 })
 Good.create({
   name: 'Cool hat',
-  shopid: '2',
+  shopid: (i+=1).to_s,
   description: 'クールな帽子',
   data: '',
   price: '3200',
@@ -20,7 +21,7 @@ Good.create({
 })
 Good.create({
   name: '不織布マスク',
-  shopid: '3',
+  shopid: (i+=1).to_s,
   description: '薄いマスク',
   data: '',
   price: '100',
@@ -28,4 +29,34 @@ Good.create({
   link: 'https://www.amazon.co.jp/',
   thumbnail: 'https://1.bp.blogspot.com/-tEEP6DMBBm4/Xtt6eQsD1JI/AAAAAAABZQQ/-bIHUTVF7gU9g8zGtfZfIzWkPuh5kKc1ACNcBGAsYHQ/s1600/medical_mask_front_view.png'
 })
+Good.create({
+  name: 'Plain glasses',
+  shopid: (i+=1).to_s,
+  description: '普通の眼鏡',
+  data: '',
+  price: '9000',
+  category: 'メガネ',
+  link: 'https://www.amazon.co.jp/',
+  thumbnail: 'https://2.bp.blogspot.com/-lJilMIL82DA/UZ2VGO8EaSI/AAAAAAAATuY/V6CdH2oYAWI/s800/megane_black.png'
+})
+Good.create({
+  name: 'Pretty glasses',
+  shopid: (i+=1).to_s,
+  description: 'カワイイ眼鏡❤',
+  data: '',
+  price: '18000',
+  category: 'メガネ',
+  link: 'https://www.amazon.co.jp/',
+  thumbnail: 'https://3.bp.blogspot.com/-abTzP7jhCXU/UZ2VHKTWgNI/AAAAAAAATvA/d28XnTwSVJY/s800/megane_pink.png'
+})
 
+Good.create({
+  name: 'Monstrous glasses',
+  shopid: (i+=1).to_s,
+  description: '邪悪な眼鏡',
+  data: '',
+  price: '12000000',
+  category: 'メガネ',
+  link: 'https://www.amazon.co.jp/',
+  thumbnail: 'https://1.bp.blogspot.com/-39LCjzW8pDA/XvcI4mEApbI/AAAAAAABZto/IOwANO0mePEoUeH6IeyOohg7gs7q2YPogCNcBGAsYHQ/s1600/megane_hikaru_man.png'
+})
